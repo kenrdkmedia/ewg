fb296eb71b885aeded8c523a273c53cae320b456
pete-ewg17 <peteakhurst@Petes-MacBook-Pro.local>
2017-02-21 15:29:29 +1100

    Added form

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,3 +1,7 @@
+/*===========
+  BACK TO TOP
+=============*/
+
 $(document).ready(function(){
      $(window).scroll(function () {
             if ($(this).scrollTop() > 50) {
@@ -19,3 +23,35 @@ $(document).ready(function(){
 
 });
 
+/*===========
+  FORM 
+=============*/
+
+$(function () {
+
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
